98a8d43d300f2827beb36b714a97aacd7b126bac
Vivek Shukla <51740903+vivekshukla24@users.noreply.github.com>
2021-12-02 01:51:30 +0530

    Add files via upload

---
+++ permutation.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class permutation {
+    public static void main(String[] args) {
+        perm("","abc");
+
+    }
+    public static void perm(String processed,String unprocessed){
+        if(unprocessed.isEmpty()){
+            System.out.println(unprocessed);
+            return;
+
+        }
+        char ch=unprocessed.charAt(0);
+        unprocessed=unprocessed.substring(1);
+        for (int i = 0; i <=processed.length() ; i++) {
+            String first = processed.substring(0,i);
+            String second = processed.substring(i);
+
+            perm(first+ch+second,unprocessed);
+
+        }
+    }
+}
